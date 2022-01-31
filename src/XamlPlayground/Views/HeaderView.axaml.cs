aad62463e2b1d7d5e12affecd4bcd1e85197fac1
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-01-31 23:12:47 +0100

    Refactor views

---
+++ src/XamlPlayground/Views/HeaderView.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace XamlPlayground.Views;
+
+public class HeaderView : UserControl
+{
+    public HeaderView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
+
