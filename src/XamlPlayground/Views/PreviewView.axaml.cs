a1c2f2dd86fd548d6c6737bc4181a306f991a6a0
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-02-01 07:32:51 +0000

    Add user controls

---
+++ src/XamlPlayground/Views/PreviewView.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace XamlPlayground.Views;
+
+public class PreviewView : UserControl
+{
+    public PreviewView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
---
+++ src/XamlPlayground/Views/XamlView.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace XamlPlayground.Views;
+
+public class XamlView : UserControl
+{
+    public XamlView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
