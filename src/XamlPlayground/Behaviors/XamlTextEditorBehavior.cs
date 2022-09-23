c50ef4009604f4d9edf914add9c7fe7b3eb00fac
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-09-23 12:03:17 +0200

    Update XamlTextEditorBehavior.cs

--- src/XamlPlayground/Behaviors/XamlTextEditorBehavior.cs
+++ src/XamlPlayground/Behaviors/XamlTextEditorBehavior.cs
@@ -1,4 +1,5 @@
-﻿using Avalonia.Xaml.Interactivity;
+﻿using System;
+using Avalonia.Xaml.Interactivity;
 using AvaloniaEdit;
 using AvaloniaEdit.Highlighting;
 using AvaloniaEdit.TextMate;
@@ -23,15 +24,22 @@ public class XamlTextEditorBehavior : Behavior<TextEditor>
             // TODO: Enable for WebAssembly
             // https://github.com/danipen/TextMateSharp/issues/9
             // https://github.com/AvaloniaUI/AvaloniaEdit/issues/201
-            if (!Utilities.IsBrowser())
+            if (Utilities.IsBrowser())
+            {
+                _textEditor.SyntaxHighlighting = HighlightingManager.Instance.GetDefinition("XML");
+
+                /*
+                _textEditor.KeyDown += (_, args) =>
+                {
+                    Console.WriteLine($"KeyDown: {args.Key.ToString()}");
+                };
+                //*/
+            }
+            else
             {
                 _registryOptions = new RegistryOptions(ThemeName.LightPlus);
                 _textMateInstallation = _textEditor.InstallTextMate(_registryOptions);
                 _textMateInstallation.SetGrammar(_registryOptions.GetScopeByLanguageId(_registryOptions.GetLanguageByExtension(".xml").Id));
-            }
-            else
-            {
-                _textEditor.SyntaxHighlighting = HighlightingManager.Instance.GetDefinition("XML");
             }
         }
     }
