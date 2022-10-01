ef6039b829bfbd40e821871fb8fcedd811a992b2
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-10-01 12:59:17 +0200

    Refactor

---
+++ src/XamlPlayground/Behaviors/TextEditorBehavior.cs
@@ -0,0 +1,55 @@
+﻿using Avalonia;
+using Avalonia.Xaml.Interactivity;
+using AvaloniaEdit;
+using AvaloniaEdit.Highlighting;
+using AvaloniaEdit.TextMate;
+using TextMateSharp.Grammars;
+
+namespace XamlPlayground.Behaviors;
+
+public class TextEditorBehavior : Behavior<TextEditor>
+{
+    public static readonly StyledProperty<string?> ExtensionProperty = 
+        AvaloniaProperty.Register<TextEditorBehavior, string?>(nameof(Extension));
+
+    public static readonly StyledProperty<bool> UseTextMateProperty = 
+        AvaloniaProperty.Register<TextEditorBehavior, bool>(nameof(UseTextMate));
+
+    private TextEditor? _textEditor;
+    private RegistryOptions? _registryOptions;
+    private TextMate.Installation? _textMateInstallation;
+
+    public string? Extension
+    {
+        get => GetValue(ExtensionProperty);
+        set => SetValue(ExtensionProperty, value);
+    }
+
+    public bool UseTextMate
+    {
+        get => GetValue(UseTextMateProperty);
+        set => SetValue(UseTextMateProperty, value);
+    }
+
+    protected override void OnAttached()
+    {
+        base.OnAttached();
+
+        if (AssociatedObject is { } textEditor)
+        {
+            _textEditor = textEditor;
+
+            if (UseTextMate)
+            {
+                _registryOptions = new RegistryOptions(ThemeName.LightPlus);
+                _textMateInstallation = _textEditor.InstallTextMate(_registryOptions);
+                _textMateInstallation.SetGrammar(
+                    _registryOptions.GetScopeByLanguageId(_registryOptions.GetLanguageByExtension(Extension).Id));
+            }
+            else
+            {
+                _textEditor.SyntaxHighlighting = HighlightingManager.Instance.GetDefinitionByExtension(Extension);
+            }
+        }
+    }
+}
