cded7926ea5e3cdab72ebc64c7f76ad357e290b6
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-10-10 21:39:53 +0200

    Update Program.cs

--- src/XamlPlayground.Wasm/Program.cs
+++ src/XamlPlayground.Wasm/Program.cs
@@ -5,9 +5,7 @@ using XamlPlayground;
 internal partial class Program
 {
     private static void Main(string[] args)
-    {
-        BuildAvaloniaApp().SetupBrowserApp("out");
-    }
+        => BuildAvaloniaApp().SetupBrowserApp("out");
 
     public static AppBuilder BuildAvaloniaApp()
            => AppBuilder.Configure<App>();
