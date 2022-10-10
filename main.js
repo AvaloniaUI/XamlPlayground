9a41f5315fb3081b4577fba957e554527cc48833
wieslawsoltes <wieslawsoltes@users.noreply.github.com>
2022-10-10 20:14:47 +0000

    Deploying to gh-pages from @ AvaloniaUI/XamlPlayground@ca0d23d1af26d806bf67452bd291d860026f75af 🚀

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
 import { dotnet } from './dotnet.js'
 import { createAvaloniaRuntime } from './avalonia.js';
 
