9ae5c253d31d9a819b3fbcc0f9b504c98181f3a7
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-10-11 20:56:50 +0200

    Update Program.cs

--- src/XamlPlayground.Wasm/Program.cs
+++ src/XamlPlayground.Wasm/Program.cs
@@ -1,22 +1,23 @@
 using System;
 using System.Runtime.InteropServices.JavaScript;
+using System.Runtime.Versioning;
 using Avalonia;
 using Avalonia.Web;
 using XamlPlayground;
 using XamlPlayground.Services;
 using XamlPlayground.Wasm;
 
+[assembly:SupportedOSPlatform("browser")]
+
 internal partial class Program
 {
     private static void Initialize()
     {
-#pragma warning disable CA1416
         _ = JSHost.ImportAsync("interop.js", "./interop.js").ContinueWith(_ =>
         {
             CompilerService.BaseUri = Interop.GetBaseUri();
             Console.WriteLine($"BaseUri: {CompilerService.BaseUri}");
         });
-#pragma warning restore CA1416
     }
     
     private static void Main(string[] args) =>
