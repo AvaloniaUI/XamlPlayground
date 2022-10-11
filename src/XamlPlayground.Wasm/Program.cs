a55052377ac4d7766e9808118c17aacf2492fa37
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-10-11 20:09:57 +0200

    Update Program.cs

--- src/XamlPlayground.Wasm/Program.cs
+++ src/XamlPlayground.Wasm/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.InteropServices.JavaScript;
 using Avalonia;
 using Avalonia.Web;
 using XamlPlayground;
@@ -7,14 +8,22 @@ using XamlPlayground.Wasm;
 
 internal partial class Program
 {
-    private static void Main(string[] args)
+    private static void Initialize()
     {
-        CompilerService.BaseUri = Interop.GetBaseUri();
-        Console.WriteLine($"BaseUri: {CompilerService.BaseUri}");
-
-        BuildAvaloniaApp().SetupBrowserApp("out");
+#pragma warning disable CA1416
+        _ = JSHost.ImportAsync("interop.js", "./interop.js").ContinueWith(_ =>
+        {
+            CompilerService.BaseUri = Interop.GetBaseUri();
+            Console.WriteLine($"BaseUri: {CompilerService.BaseUri}");
+        });
+#pragma warning restore CA1416
     }
+    
+    private static void Main(string[] args) =>
+        BuildAvaloniaApp()
+            .AfterSetup(_ => Initialize())
+            .SetupBrowserApp("out");
 
     public static AppBuilder BuildAvaloniaApp()
-           => AppBuilder.Configure<App>();
+        => AppBuilder.Configure<App>();
 }
